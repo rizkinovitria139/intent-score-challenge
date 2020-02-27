004fe277e921ada1720ea62c8de9471a55159a12
kiki <rahmarizki37@gmail.com>
2020-02-27 04:37:31 -0800

    menambahkan atribut onClick pada scorer activity

--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScorerActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ScorerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+    }
+
+    public void handleAddScore(View view) {
+        Intent intent = new Intent(this, MatchActivity.class);
+        startActivity(intent);
     }
 }
